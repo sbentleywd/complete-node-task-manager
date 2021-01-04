dca24bac50f705573b69dc16b4b7aa40b421deca
Simon Bentley <61815607+sbentleywd@users.noreply.github.com>
2021-01-04 21:00:19 +0000

    adds filter to get tasks endpoint

--- src/models/user.js
+++ src/models/user.js
@@ -4,53 +4,58 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Task = require("./task");
 
-const userSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-		trim: true,
-	},
-	age: {
-		type: Number,
-		default: 0,
-		validate(value) {
-			if (value < 0) {
-				throw new Error("Age must be a positive number");
-			}
+const userSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			trim: true,
 		},
-	},
-	email: {
-		type: String,
-		required: true,
-		unique: true,
-		trim: true,
-		lowercase: true,
-		validate(value) {
-			if (!validator.isEmail(value)) {
-				throw new Error("Email is invalid");
-			}
-		},
-	},
-	password: {
-		type: String,
-		required: true,
-		minLength: 7,
-		trim: true,
-		validate(value) {
-			if (value.toLowerCase().includes("password")) {
-				throw new Error('Password cannot contain "password"');
-			}
-		},
-	},
-	tokens: [
-		{
-			token: {
-				type: String,
-				required: true,
+		age: {
+			type: Number,
+			default: 0,
+			validate(value) {
+				if (value < 0) {
+					throw new Error("Age must be a positive number");
+				}
 			},
 		},
-	],
-});
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+			trim: true,
+			lowercase: true,
+			validate(value) {
+				if (!validator.isEmail(value)) {
+					throw new Error("Email is invalid");
+				}
+			},
+		},
+		password: {
+			type: String,
+			required: true,
+			minLength: 7,
+			trim: true,
+			validate(value) {
+				if (value.toLowerCase().includes("password")) {
+					throw new Error('Password cannot contain "password"');
+				}
+			},
+		},
+		tokens: [
+			{
+				token: {
+					type: String,
+					required: true,
+				},
+			},
+		],
+	},
+	{
+		timestamps: true,
+	}
+);
 
 userSchema.virtual("tasks", {
 	ref: "Task",
