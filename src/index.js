6b5a540b5bb371c48cac3fd8bf5031e40420ece7
Simon Bentley <61815607+sbentleywd@users.noreply.github.com>
2021-01-04 16:48:16 +0000

    adds task/user relationship

--- src/index.js
+++ src/index.js
@@ -22,3 +22,20 @@ app.use(taskRouter);
 app.listen(port, () => {
 	console.log(`Server is up on ${port}`);
 });
+
+const Task = require("./models/task");
+const User = require("./models/user");
+
+const main = async () => {
+	// find owner of a task from task id
+	// const task = await Task.findById("5ff342e44a7f221e90dd4478");
+	// await task.populate("owner").execPopulate();
+	// console.log(task.owner);
+
+	// find tasks associated with an owner
+	const user = await User.findById("5ff342193ad1731dd24cd2a9");
+	await user.populate("tasks").execPopulate();
+	console.log(user.tasks);
+};
+
+main();
