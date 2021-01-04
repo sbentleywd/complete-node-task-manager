44829dd8d7babbacc841e8f5340cfc5537bb52a0
Simon Bentley <61815607+sbentleywd@users.noreply.github.com>
2021-01-04 15:48:58 +0000

    adds logout endpoints

--- src/routers/user.js
+++ src/routers/user.js
@@ -71,6 +71,28 @@ router.post("/users/login", async (req, res) => {
 	}
 });
 
+router.post("/users/logout", auth, async (req, res) => {
+	try {
+		req.user.tokens = req.user.tokens.filter((token) => {
+			return token.token !== req.token;
+		});
+		await req.user.save();
+		res.send();
+	} catch (e) {
+		res.status(500).send();
+	}
+});
+
+router.post("/users/logout/all", auth, async (req, res) => {
+	try {
+		req.user.tokens = [];
+		await req.user.save();
+		res.send();
+	} catch (e) {
+		res.status(500).send();
+	}
+});
+
 router.delete("/users/:id", async (req, res) => {
 	try {
 		const user = await User.findByIdAndDelete(req.params.id);
