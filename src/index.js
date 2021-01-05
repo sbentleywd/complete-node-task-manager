8e52432b40105caf3071df224dff5efae8437d3f
Simon Bentley <61815607+sbentleywd@users.noreply.github.com>
2021-01-05 14:10:45 +0000

    adds file restrictions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
-
+const multer = require("multer");
 require("./db/mongoose");
 
 const userRouter = require("./routers/user");
@@ -8,6 +8,23 @@ const taskRouter = require("./routers/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const upload = multer({
+	dest: "images",
+	limits: {
+		fileSize: 1000000, // 1MB
+	},
+	fileFilter(req, file, cb) {
+		if (!file.originalname.match(/\.(doc|docx)/)) {
+			return cb(new Error("please upload a word document"));
+		}
+		cb(undefined, true);
+	},
+});
+
+app.post("/upload", upload.single("upload"), (req, res) => {
+	res.send();
+});
 
 app.use(express.json());
 app.use(morgan("dev"));
