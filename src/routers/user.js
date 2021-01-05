808c015a2561d0ec26ba7cef525b0b50a64a1007
Simon Bentley <61815607+sbentleywd@users.noreply.github.com>
2021-01-05 17:15:27 +0000

    adds sharp image conversion & resize

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,7 @@ const router = new express.Router();
 const User = require("../models/user");
 const auth = require("../middleware/auth");
 const multer = require("multer");
+const sharp = require("sharp");
 
 router.get("/users/me", auth, async (req, res) => {
 	res.send(req.user);
@@ -100,7 +101,11 @@ router.post(
 	auth,
 	upload.single("avatar"),
 	async (req, res) => {
-		req.user.avatar = req.file.buffer;
+		const buffer = await sharp(req.file.buffer)
+			.resize({ width: 250, height: 250 })
+			.png()
+			.toBuffer();
+		req.user.avatar = buffer;
 		await req.user.save();
 		res.send();
 	},
@@ -122,7 +127,7 @@ router.get("/users/:id/avatar", async (req, res) => {
 			throw new Error();
 		}
 
-		res.set("Content-Type", "image/jpg");
+		res.set("Content-Type", "image/png");
 		res.send(user.avatar);
 	} catch (e) {
 		res.status(404).send();
