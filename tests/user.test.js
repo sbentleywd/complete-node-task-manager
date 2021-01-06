411bb1b224cadf9f47f93a3e8ffc525e2059cb21
Simon Bentley <61815607+sbentleywd@users.noreply.github.com>
2021-01-06 15:47:30 +0000

    login tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,6 +1,17 @@
-const supertest = require("supertest");
 const request = require("supertest");
 const app = require("../src/app");
+const User = require("../src/models/user");
+
+const user1 = {
+	name: "Gary Neville",
+	email: "gaz@example.com",
+	password: "rightback2",
+};
+
+beforeEach(async () => {
+	await User.deleteMany();
+	await new User(user1).save();
+});
 
 test("should sign up a new user", async () => {
 	await request(app)
@@ -11,4 +22,24 @@ test("should sign up a new user", async () => {
 			password: "gibbon123",
 		})
 		.expect(201);
+});
+
+test("Should log in existing user", async () => {
+	await request(app)
+		.post("/users/login")
+		.send({
+			email: user1.email,
+			password: user1.password,
+		})
+		.expect(200);
+});
+
+test("Should not log in non existant user", async () => {
+	await request(app)
+		.post("/users/login")
+		.send({
+			email: "blah@blah.com",
+			password: "blahblahblah",
+		})
+		.expect(400);
 });
